eb4181c276107dfd1bb84301e72ad1b277a26fd9
dockbearolda <dockbear@gmail.com>
2026-04-11 15:57:29 -0400

    Supprime le bouton Mes Projets de la navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,10 @@ import { Manrope } from "next/font/google";
 
 import { CartButton } from "@/components/cart/cart-button";
 import { CartDrawer } from "@/components/cart/cart-drawer";
-import { ProjectsDrawer } from "@/components/projects/projects-drawer";
-import { ProjectsNavButton } from "@/components/projects/projects-nav-button";
+
 import { CartProvider } from "@/lib/cart-context";
 import { CartDrawerProvider } from "@/lib/cart-drawer-context";
-import { ProjectsProvider } from "@/lib/projects-context";
-import { ProjectsDrawerProvider } from "@/lib/projects-drawer-context";
+
 import { ToastProvider } from "@/lib/toast-context";
 import { AuthSessionProvider } from "@/lib/session-provider";
 import { siteConfig } from "@/data/site";
@@ -61,19 +59,13 @@ export default function RootLayout({
            * car ProjectsProvider consomme useCart() en interne.
            */}
           <CartProvider>
-            <ProjectsProvider>
               <CartDrawerProvider>
-                <ProjectsDrawerProvider>
                   <ToastProvider>
                     {children}
                     <CartButton />
                     <CartDrawer />
-                    <ProjectsNavButton />
-                    <ProjectsDrawer />
                   </ToastProvider>
-                </ProjectsDrawerProvider>
               </CartDrawerProvider>
-            </ProjectsProvider>
           </CartProvider>
         </AuthSessionProvider>
       </body>
